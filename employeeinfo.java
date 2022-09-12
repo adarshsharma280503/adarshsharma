e055fa49b706ce8f1ec2075c05d2823dd12c8bd9
adarshsharma280503 <112937545+adarshsharma280503@users.noreply.github.com>
2022-09-12 21:36:36 +0530

    Add files via upload

---
+++ employeeinfo.java
@@ -0,0 +1,27 @@
+class employeeinfo
+{
+public static void main (String args[]) 
+
+{
+employees e1= new employees();
+employees e2= new employees();
+employees e3= new employees();
+
+e1.name="Adarsh";
+e1.id= 1;
+e1.salary= 25000;
+
+e2.name="masud";
+e2.id= 2;
+e2.salary= 32000;
+
+e3.name="priya";
+e3.id= 3;
+e3.salary= 30000;
+
+e1.employeeM1();
+e2.employeeM1();
+e3.employeeM1();
+}
+}
+
---
+++ employees.java
@@ -0,0 +1,13 @@
+class employees
+{
+static String company= "BKT ";
+static String product= "TYRE ";
+int id;
+String name;
+int salary;
+
+void employeeM1()
+{
+System.out.println("company name : "+company+" product : "+product+" id of employee: "+ id +" name of employee : "+ name +" salary of employee : "+ salary);
+}
+}
