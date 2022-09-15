754827753cfd9fd4b1404dfd57497e6d3c469163
adarshsharma280503 <112937545+adarshsharma280503@users.noreply.github.com>
2022-09-15 14:59:26 +0530

    Add files via upload

---
+++ Companyemployee.java
@@ -0,0 +1,16 @@
+class Companyemployee
+{
+public static void main (String args[])
+{
+int e = 30000;
+int e1 = 11000;
+int e2 = 5000;
+int e3 = e-e1-e2 ;
+{
+System.out.println("the amount debit from company to pay the salary of employee is" + " " + e);
+System.out.println("the salary of first employee is" + " " + e1);
+System.out.println("the salary of second employee is" + " " + e2);
+System.out.println("the salary of third employee is" + " " + e3);
+}
+}
+}
