58d1de1f4d8bbb6cb240ef79460ff484a4f3bae9
adarshsharma280503 <112937545+adarshsharma280503@users.noreply.github.com>
2022-09-15 11:55:20 +0530

    Add files via upload

---
+++ mainoverloading.java
@@ -0,0 +1,14 @@
+class mainoverloading
+{
+public static void main(String args[])
+{
+overloading test= new overloading();
+int a = 10;
+String w = "streat goavil road";
+String c = " titwala police station";
+test.adarsh(a, w, c);
+test.adarsh(a, w);
+test.adarsh(a);
+test.adarsh();
+}
+}
---
+++ overloading.java
@@ -0,0 +1,19 @@
+class overloading
+{
+void adarsh()
+{
+System.out.println(" this is my address");
+}
+void adarsh(int i)
+{
+System.out.println(i + " " + " this is my address");
+}
+void adarsh(int i, String a)
+{
+System.out.println(i + " " + a + " " + " this is my address");
+}
+void adarsh(int i, String a, String b)
+{
+System.out.println(i + " " + a + " " + b +" " + " this is my address");
+}
+}
