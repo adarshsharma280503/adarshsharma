f4813799013c95662dea47d093a5af6a86ce3c28
adarshsharma280503 <112937545+adarshsharma280503@users.noreply.github.com>
2022-10-07 11:03:55 +0530

    Add files via upload

---
+++ class partical/getter setter/Encapsulaction/AdarshEncapsulaction.java
@@ -0,0 +1,38 @@
+package Encapsulaction;
+
+class AdarshEncapsulaction {
+	
+	int age;
+	String name;
+	String father;
+	String mother;
+	//getter setter method
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFather() {
+		return father;
+	}
+	public void setFather(String father) {
+		this.father = father;
+	}
+	public String getMother() {
+		return mother;
+	}
+	public void setMother(String mother) {
+		this.mother = mother;
+	}
+	
+	
+	
+
+}
---
+++ class partical/getter setter/Encapsulaction/MainAdarshEncapsulaction.java
@@ -0,0 +1,24 @@
+package Encapsulaction;
+
+class MainAdarshEncapsulaction {
+	 
+	public static void main(String args[]) {
+	  
+	AdarshEncapsulaction e = new AdarshEncapsulaction();
+	e.setAge(20);
+	e.setName(" Adarsh");
+	e.setFather("Lalji");
+	e.setMother("sushma");
+	
+	System.out.println("age is : "+ e.getAge());
+	System.out.println("my name is : "+ e.getName());
+	System.out.println("father name : " + e.getFather());
+	System.out.println("mother name : " + e.getMother());
+	
+	
+	
+	} 
+	
+
+}
+ 
