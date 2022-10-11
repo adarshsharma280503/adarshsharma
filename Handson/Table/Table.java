7af24fb314c081a94d084807fbaa8a52c6abb018
adarshsharma280503 <112937545+adarshsharma280503@users.noreply.github.com>
2022-10-11 11:35:57 +0530

    Add files via upload

---
+++ Handson/Table/Table.java
@@ -0,0 +1,19 @@
+package javaprojrct;
+
+import java.util.Scanner;
+
+public class Table {
+	
+public static void main (String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number: ");
+		int a = sc.nextInt();
+		
+		for(int i=1; i<=10; i++)		
+				{
+				System.out.println( a+" X "+i +" = "+a*i );
+				}
+				}
+
+}
